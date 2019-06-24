31a3eee3f69c95cc8f5afe57edf27c4f1c39c8f3
bta134 <bta134@gmail.com>
2019-06-24 10:56:53 +0700

    Update SampleService.java

--- WSRestService/src/main/java/org/rest/service/entities/SampleService.java
+++ WSRestService/src/main/java/org/rest/service/entities/SampleService.java
@@ -10,8 +10,8 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.logging;
+import java.util.logging.*;
 
     
 @Path("/sampleservice")
