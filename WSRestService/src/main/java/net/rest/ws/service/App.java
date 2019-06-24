53560af17811971539d016481d875b29594d589f
bta134@gmail.com <bta134@gmail.com>
2019-06-24 02:45:36 +0000

    WSRestService

---
+++ WSRestService/src/main/java/net/rest/ws/service/App.java
@@ -0,0 +1,13 @@
+package net.rest.ws.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
